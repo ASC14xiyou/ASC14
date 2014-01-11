3583c6157c974b10bc1d35c5e2d42ffc12146913
wanghuajie <whj@ubuntu.(none)>
2014-01-11 14:25:18 +0800

    whj

---
+++ whj.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+        > File Name: whj.c
+        > Author: HarryKing
+        > Mail: wanghuajie1994@gmail.com 
+        > Created Time: Sat 11 Jan 2014 02:23:34 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int 
+main(void)
+{
+    printf("hello world\n");
+    return 0;
+}
