efb924198f151f6ec2401fb7c3ef9743bf2d62c6
wanghuajie <whj@ubuntu.(none)>
2014-01-11 15:01:08 +0800

    wang.c

---
+++ wang.c
@@ -0,0 +1,9 @@
+/*************************************************************************
+        > File Name: wang.c
+        > Author: HarryKing
+        > Mail: wanghuajie1994@gmail.com 
+        > Created Time: Sat 11 Jan 2014 03:00:39 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+wang.c
